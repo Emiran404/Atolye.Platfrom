71ab9af444504d49f73ded133350b8bf3d97fee4
Emirhan Gök <75798978+Emiran404@users.noreply.github.com>
2026-04-12 20:50:00 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig(({ command }) => ({
   },
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 80,
     watch: {
       ignored: ['**/src/uploads_student/**']
     },
